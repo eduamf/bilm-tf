20dc54d5cc48cd279bf3b8ffb286641769684b62
Andrey Kutuzov <akutuzov72@gmail.com>
2019-06-24 02:52:49 +0200

    Script to extract vocabulary

---
+++ vocabulary.py
@@ -0,0 +1,21 @@
+#!/projects/ltg/python3/bin/python3
+import sys
+
+THRESHOLD = int(sys.argv[1])
+
+words = {}
+
+for line in sys.stdin:
+    res = line.strip().split()
+    for word in res:
+        if not word in words:
+            words[word] = 0
+        words[word] += 1
+
+print('\n'.join(['<S>', '</S>', '<UNK>']))
+
+print('Vocabulary:', len(words), file=sys.stderr)
+
+a = sorted(words, key=words.get, reverse=True)[:THRESHOLD]
+for w in a:
+    print(w)
