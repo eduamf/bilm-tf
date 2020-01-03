e8f58c4e0e211028e6b5b23df60ee0298c2307b2
eduamf <eduardo.freitas@camara.leg.br>
2020-01-03 00:54:23 +0000

    Update.

--- bilm/training.py
+++ bilm/training.py
@@ -1030,6 +1030,96 @@ def test(options, ckpt_file, data, batch_size=256):
 
     return np.exp(avg_loss)
 
+def tag(options, ckpt_file, shards, datasets, batch_size=1):
+    '''
+    Get perplexity of each word.
+    '''
+
+    bidirectional = options.get('bidirectional', False)
+    char_inputs = 'char_cnn' in options
+    if char_inputs:
+        max_chars = options['char_cnn']['max_characters_per_token']
+
+    unroll_steps = 1
+
+    config = tf.ConfigProto(allow_soft_placement=True)
+    with tf.Session(config=config) as sess:
+        with tf.device('/gpu:0'), tf.variable_scope('lm'):
+            test_options = dict(options)
+            # NOTE: the number of tokens we skip in the last incomplete
+            # batch is bounded above batch_size * unroll_steps
+            test_options['batch_size'] = batch_size
+            test_options['unroll_steps'] = 1
+            model = LanguageModel(test_options, False)
+            # we use the "Saver" class to load the variables
+            loader = tf.train.Saver()
+            loader.restore(sess, ckpt_file)
+
+        # model.total_loss is the op to compute the loss
+        # perplexity is exp(loss)
+        init_state_tensors = model.init_lstm_state
+        final_state_tensors = model.final_lstm_state
+        if not char_inputs:
+            feed_dict = {
+                model.token_ids:
+                        np.zeros([batch_size, unroll_steps], dtype=np.int64)
+            }
+            if bidirectional:
+                feed_dict.update({
+                    model.token_ids_reverse:
+                        np.zeros([batch_size, unroll_steps], dtype=np.int64)
+                })
+        else:
+            feed_dict = {
+                model.tokens_characters:
+                   np.zeros([batch_size, unroll_steps, max_chars],
+                                 dtype=np.int32)
+            }
+            if bidirectional:
+                feed_dict.update({
+                    model.tokens_characters_reverse:
+                        np.zeros([batch_size, unroll_steps, max_chars],
+                            dtype=np.int32)
+                })
+
+        init_state_values = sess.run(
+            init_state_tensors,
+            feed_dict=feed_dict)
+
+        for dataset_no, (shard, data) in enumerate(zip(shards, datasets)):
+            print(dataset_no, end=': ')
+            t1 = time.time()
+            batch_losses = []
+            total_loss = 0.0
+            perplexity_array = []
+            for batch_no, batch in enumerate(
+                                    data.iter_batches(batch_size, 1), start=1):
+                # slice the input in the batch for the feed_dict
+                X = batch
+
+                feed_dict = {t: v for t, v in zip(
+                                            init_state_tensors, init_state_values)}
+
+                feed_dict.update(
+                    _get_feed_dict_from_X(X, 0, X['token_ids'].shape[0], model,
+                                              char_inputs, bidirectional)
+                )
+
+                ret = sess.run(
+                    [model.total_loss, final_state_tensors],
+                    feed_dict=feed_dict
+                )
+
+                loss, init_state_values = ret
+                batch_losses.append(loss)
+                batch_perplexity = np.exp(loss)
+                # total_loss += loss
+                # avg_perplexity = np.exp(total_loss / batch_no)
+                perplexity_array.append(batch_perplexity)
+
+            avg_loss = np.mean(batch_losses)
+            avg_perplexity = np.exp(avg_loss)
+            print(avg_perplexity)
 
 def load_options_latest_checkpoint(tf_save_dir):
     options_file = os.path.join(tf_save_dir, 'options.json')
