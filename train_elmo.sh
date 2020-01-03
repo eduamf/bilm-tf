5641c50cad02e52cd33aa0268dacacc89e26943a
eduamf <eduardo.freitas@camara.leg.br>
2020-01-03 03:04:29 +0000

    Added log_dir

--- train_elmo.sh
+++ train_elmo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export CUDA_VISIBLE_DEVICES=2,3
+export CUDA_VISIBLE_DEVICES=0
 DATA=${1}
 VOCAB=${2}
 echo 'Counting tokens in the training corpora...'
@@ -9,4 +9,4 @@ SIZE=`zcat $DATA*.gz | wc -w`
 echo $VOCAB
 echo $SIZE
 
-python3 bin/train_elmo.py --train_prefix ${1}  --vocab_file ${2} --save_dir ${3} --size $SIZE
+python3 bin/train_elmo.py --train_prefix ${1}  --vocab_file ${2} --save_dir ${3} --log_dir $(4)--size $SIZE
